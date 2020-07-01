f6ca404509dc8dff047ffa28c584581d637b0ac2
AhmedEssam07 <45883944+AhmedEssam07@users.noreply.github.com>
2020-07-01 21:49:43 +0200

    Add files via upload

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,32 @@
-## Put comments here that give an overall description of what your
-## functions do
+# cm stands for Cashed matrix 
 
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  cm <- NULL
+  set <- function(y) {
+    x <<- y
+    cm <<- NULL
+  }
+  get <- function() x
+  setCM <- function(solve) cm <<- solve
+  getinverse <- function() cm
+  list(set = set, get = get,
+       setCM = setCM,
+       getinverse = getinverse)
 }
 
 
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  cm <- x$getinverse()
+  if(!is.null(cm)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  cm <- solve(data, ...)
+  x$setCM(cm)
+  cm
 }
